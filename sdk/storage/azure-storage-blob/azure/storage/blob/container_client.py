f903f81fd3bd25accee0a2d5de57fcb0e3008caf
Laurent Mazuel <laurent.mazuel@gmail.com>
2019-09-19 16:40:44 -0700

    Storage WIP

--- sdk/storage/azure-storage-blob/azure/storage/blob/container_client.py
+++ sdk/storage/azure-storage-blob/azure/storage/blob/container_client.py
@@ -1062,6 +1062,8 @@ class ContainerClient(StorageAccountHostsMixin):
         except StorageErrorException as error:
             process_storage_error(error)
 
+        return pipeline_response.context['MULTIPART_RESPONSE']
+
     def get_blob_client(
             self, blob,  # type: Union[str, BlobProperties]
             snapshot=None  # type: str
